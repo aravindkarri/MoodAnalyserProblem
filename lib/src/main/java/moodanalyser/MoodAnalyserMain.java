f557f39c9cf34c6cf401827f80b481117b1c29ac
aravindkarri <aravindkarri98495@gmail.com>
2021-07-17 17:54:18 +0530

    ADDED: Initial setup for mood analyser

---
+++ lib/src/main/java/moodanalyser/MoodAnalyserMain.java
@@ -0,0 +1,10 @@
+package moodanalyser;
+
+public class MoodAnalyserMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ lib/src/test/java/moodanalysertesting/MoodAnalyserTest.java
@@ -0,0 +1,5 @@
+package moodanalysertesting;
+
+public class MoodAnalyserTest {
+
+}
