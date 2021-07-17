70ef5bfa47b286ea1d9de85ecee5a813bfbfae55
aravindkarri <aravindkarri98495@gmail.com>
2021-07-17 17:59:04 +0530

    ADDED: check for happy or sad mood

--- lib/src/main/java/moodanalyser/MoodAnalyserMain.java
+++ lib/src/main/java/moodanalyser/MoodAnalyserMain.java
@@ -1,10 +1,17 @@
 package moodanalyser;
 
-public class MoodAnalyserMain {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class MoodAnalyserMain 
+{
+	//Method to return mood based on message
+	public String analyseMood(String message)
+	{
+		if(message.contains("sad"))
+		{
+			return "SAD";
+		}
+		else
+		{
+			return "HAPPY";
+		}
 	}
-
 }
