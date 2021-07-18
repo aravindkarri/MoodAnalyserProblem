6d05aaddfae9f131ff3d51ed4af4b6787d9a5279
aravindkarri <aravindkarri98495@gmail.com>
2021-07-18 12:09:33 +0530

    Refactored: code to take mood message in constructor

--- lib/src/main/java/moodanalyser/MoodAnalyser.java
+++ lib/src/main/java/moodanalyser/MoodAnalyser.java
@@ -2,6 +2,17 @@ package moodanalyser;
 
 public class MoodAnalyser
 {
+	String message;
+	//Parameterize constructor
+	public MoodAnalyser(String message)
+	{
+		this.message = message;
+	}
+	//default constructor
+	public MoodAnalyser()
+	{
+		super();
+	}
 	//Method to return mood based on message
 	public String analyseMood(String message)
 	{
